08bb6484b1e2c493633d995d8a181b32b29d9915
PoshikaRajpurohit <159758700+PoshikaRajpurohit@users.noreply.github.com>
2024-12-24 10:20:04 +0530

    Add files via upload

---
+++ AINCV.js
@@ -0,0 +1,42 @@
+// ARROW-FUNCTIONS
+// let a = () => console.log("Hello!Everyone")
+// a();
+
+
+// IIEF
+// (function (){
+//  console.log("IIEF.....");
+//  })()
+
+
+
+//  NESTED-FUNCTION
+// function OUTER(a,b){
+//     console.log("Division=",a/b);
+//     function INNER(){
+//     console.log("module=",a%b);
+//     }
+//     INNER()
+// }
+// OUTER(4,2);
+
+
+
+// CLOSURE-FUNCTION
+// function OUTER(a,b){
+//     console.log("Division=",a/b);
+//     function INNER(){
+//     console.log("module=",a%b);
+//     }
+//     return INNER;
+// }
+// OUTER(4,2)();
+
+
+
+// Variable as a function
+// let a= function(a,b){
+//     console.log("The Product of a and b=",a*b);
+    
+// }
+// a(20,50);
